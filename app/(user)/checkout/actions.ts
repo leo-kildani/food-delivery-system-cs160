2fc6f3f19dafc23848c245110bfe4c3f2aa5a138
vihashah <viha.shah@sjsu.edu>
2025-10-17 23:57:47 -0700

    use state pipeline from client to server works

--- app/(user)/checkout/actions.ts
+++ app/(user)/checkout/actions.ts
@@ -1,6 +1,6 @@
-'use server';
+'use server'
 
-import {convertSegmentPathToStaticExportFilename} from 'next/dist/shared/lib/segment-cache/segment-value-encoding';
+import{convertSegmentPathToStaticExportFilename} from 'next/dist/shared/lib/segment-cache/segment-value-encoding';
 import prisma from '../../../lib/prisma';
 import {createClient} from '../../../lib/supabase/server';
 import {Product} from '@prisma/client';
@@ -22,12 +22,36 @@ export async function getCartItems() {
   let cart_items = await prisma.cartItem.findMany({where: {cartId: cartId?.id}})
   // console.log(cart_items);
   let productPromises = cart_items.map(async (cart_item) => {
+    const product =
+        await prisma.product.findUnique({where: {id: cart_item.productId}});
     return {
-      product:
-          await prisma.product.findUnique({where: {id: cart_item.productId}}),
+      product: product ? {
+        ...product,
+        pricePerUnit: product.pricePerUnit.toNumber(),
+        weightPerUnit: product.weightPerUnit.toNumber()
+      } :
+                         null,
       quantity: cart_item.quantity
     };
   })
   let products = await Promise.all(productPromises);
   return products;
+}
+export type CheckoutState = {
+  ok?: boolean;
+  formError?: string;
+  fieldErrors?: Record<string, string[]>;
+};
+export async function checkoutAction(formData: FormData) {
+  console.log('here another')
+  try {
+    const selectedItemsJson = formData.get('selectedItems') as string;
+    const selectedItems = JSON.parse(selectedItemsJson);
+    console.log(selectedItems)
+    console.log('processing checkout')
+    return {ok: true};
+  } catch (error) {
+    console.log('some error:', error);
+    return {formError: 'Checkout failed, some problem occured'};
+  }
 }
