753448c4da29dcaef8ceb72cda9a627e2a09a836
leo kildani <kildani.leonardo@gmail.com>
2025-10-21 11:52:42 -0700

    replace search functionality and clean home page

--- app/(user)/home/actions.ts
+++ app/(user)/home/actions.ts
@@ -1,29 +1,17 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { Product } from "@prisma/client";
 
-export interface ProductWithNumbers {
-  id: number;
-  name: string;
-  description: string;
-  category: string;
+export async function getProducts(): Promise<Product[]> {
+  return await prisma.product.findMany();
+}
+
+// --- SERVER ACTION ---
+export type SerializedProduct = Omit<
+  Product,
+  "pricePerUnit" | "weightPerUnit"
+> & {
   pricePerUnit: number;
   weightPerUnit: number;
-  quantityOnHand: number;
-  imageUrl: string | null;
-}
-
-export async function getProducts(): Promise<ProductWithNumbers[]> {
-  const products = await prisma.product.findMany();
-  
-  return products.map(product => ({
-    id: product.id,
-    name: product.name,
-    description: product.description,
-    category: product.category,
-    pricePerUnit: product.pricePerUnit.toNumber(),
-    weightPerUnit: product.weightPerUnit.toNumber(),
-    quantityOnHand: product.quantityOnHand,
-    imageUrl: product.imageUrl
-  }));
-}
+};
