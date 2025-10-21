c4b185ae695e375d96ab7b21c10c9a63f35e9b7f
leo-kildani <kildani.leonardo@gmail.com>
2025-10-21 12:07:27 -0700

    add navlink to account

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -34,6 +34,12 @@ export default function UserLayout({
               >
                 Home
               </Link>
+              <Link
+                href="/account/details"
+                className="text-white hover:text-blue-100 px-4 py-2 rounded-lg text-sm font-medium transition-colors duration-200 hover:bg-white/10"
+              >
+                Account
+              </Link>
               <ShoppingCartButton></ShoppingCartButton>
               <LogoutButton></LogoutButton>
             </div>
