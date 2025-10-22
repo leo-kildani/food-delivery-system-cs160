f2e6238fedd82f6dfcb026154eb41e5a34d94c3b
leo-kildani <kildani.leonardo@gmail.com>
2025-10-22 13:53:32 -0700

    checkpoint: address-form.tsx

--- app/(user)/account/addresses/address-form.tsx
+++ app/(user)/account/addresses/address-form.tsx
@@ -1,30 +1,111 @@
 "use client";
 
-import { useActionState, useEffect, useState } from "react";
+import { useActionState, useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { X } from "lucide-react";
 import { addAddressAction, DeliveryAddressState } from "./actions";
-import { User } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
+import {setOptions, importLibrary} from "@googlemaps/js-api-loader"
+import { RADIUS_METERS, STORE_LOCATION } from "@/lib/constants";
 
 interface AddressFormProps {
   user: User;
 }
 
 export function AddressForm({ user }: AddressFormProps) {
+  // required local states
   const [isOpen, setIsOpen] = useState(false);
+  const [parsedAddress, setParsedAddress] =
+    useState<Prisma.DeliveryAddressCreateInput | null>(null);
+  const [mapsError, setMapsError] = useState("");
+  const [mapsLoaded, setMapsLoaded] = useState(false);
+
+  // save delivery address server action
   const [state, formAction, isPending] = useActionState(
     async (prevState: DeliveryAddressState, formData: FormData) => {
       const newState = await addAddressAction(prevState, formData);
       if (newState.ok) {
         setIsOpen(false);
+        setParsedAddress(null);
+        setMapsError("");
       }
       return newState;
     },
     {} as DeliveryAddressState
   );
+
+  // reference Maps html elements
+  const mapRef = useRef<HTMLDivElement>(null);
+  const mapsInstanceRef = useRef<google.maps.Map | null>(null);
+  const autocompleteContainerRef = useRef<HTMLDivElement>(null);
+  const placesAutocompleteRef = useRef<any>(null);
+
+  // retrieve Google Maps HTML elements on re-renders
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const initMaps = async () => {
+      try {
+        setOptions({
+          key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+          v: "weekly"
+        })
+        const {Map} = await importLibrary("maps")
+        const {AdvancedMarkerElement} = await importLibrary("marker")
+
+        // Initialize map
+        if (mapRef.current && !mapsInstanceRef.current) {
+          const map = new Map(mapRef.current, {
+            center: STORE_LOCATION,
+            zoom: 12,
+            mapId: "ADDRESS_MAP"
+          });
+          mapsInstanceRef.current = map
+
+          // Add store marker
+          new AdvancedMarkerElement({
+            map,
+            position: STORE_LOCATION,
+            title: "Store Location"
+          })
+
+          // Add circle radius of possible deliveries
+          new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            map,
+            center: STORE_LOCATION,
+            radius: RADIUS_METERS
+          })
+        }
+
+        // Initialize autocomplete
+        if (autocompleteContainerRef.current && !placesAutocompleteRef.current) {
+          const circle = new google.maps.Circle({
+            center: STORE_LOCATION,
+            radius: RADIUS_METERS
+          })
+
+          // https://developers.google.com/maps/documentation/javascript/place-autocomplete-new
+          await google.maps.importLibrary("places") as google.maps.PlacesLibrary;
+          // @ts-ignore
+          const placeAutocomplete = new google.maps.places.PlaceAutocompleteElement({
+            componentRestrictions: {country: "us"},
+            locationRestriction: circle.getBounds()
+          });
+
+          // LEFT OFF HERE
+  
+        }
+      }
+    }
+  });
 
   const handleClose = () => {
     setIsOpen(false);
