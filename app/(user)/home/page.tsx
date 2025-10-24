5b13ac49b8eddc2fb64c713fb24b312c56329330
Viha Shah <47764272+Viha123@users.noreply.github.com>
2025-10-23 22:57:28 -0700

    fixed update cart (#32)
    * made middleware admin check use absolute url on redirect
    
    * basic admin navbar (copied)
    
    * fixed update cart
    
    * Refactored product card boolean, added In Cart text
    
    ---------
    
    Co-authored-by: Ausxh <ausxh66@gmail.com>

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -21,7 +21,6 @@ export default async function HomePage() {
     pricePerUnit: p.pricePerUnit.toNumber(),
     weightPerUnit: p.weightPerUnit.toNumber(),
   }));
-
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Products</h1>
