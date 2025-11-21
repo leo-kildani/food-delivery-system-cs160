e2f4a690181404c1934a69ba1550c7aaa01f5618
leo-kildani <kildani.leonardo@gmail.com>
2025-11-21 14:21:04 -0800

    add empl and admin verification to api route

--- app/api/orders/update-etas/route.ts
+++ app/api/orders/update-etas/route.ts
@@ -1,7 +1,24 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { createClient } from "@/lib/supabase/server";
 
 export async function POST(request: NextRequest) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (
+    !user ||
+    user.user_metadata?.role !== "admin" ||
+    user.user_metadata?.role !== "empl"
+  ) {
+    return NextResponse.json(
+      { error: "Forbidden, Admin Only" },
+      { status: 403 }
+    );
+  }
   try {
     const { orderETAs } = (await request.json()) as {
       orderETAs: Array<{ orderId: number; etaMinutes: number }>;
